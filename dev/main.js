4989da1e352e3de4f38d0ab9372aa165213e334a
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-22 23:57:03 +0200

    added controls html/css, record is working, logic has now to be redefined

--- dev/main.js
+++ dev/main.js
@@ -1,23 +1,32 @@
 import Canvas from "@creenv/canvas";
 import Creenv from "@creenv/core";
+import HUD from "@creenv/hud";
+import Stats from "@creenv/stats";
 import Capture from "../lib/index";
 
 
+let hud = new HUD();
+let stats = new Stats();
+hud.add(stats);
 
 let cvs = new Canvas();
 
 
 class project extends Creenv {
   render () {
-    //cvs.background("#0000ff");
-    cvs.background("#ffffff");
-    cvs.fillStyle("#efefef");
-    cvs.rect(Math.cos(this.elapsedTime/500)*50+window.innerWidth/2-30, window.innerHeight/2-30, 60, 60);
+    stats.begin();
+    console.log(this.elapsedTime);
+    cvs.background("#0000ff");
+    //cvs.background("#ffffff");
+    //cvs.fillStyle("#efefef");
+    cvs.fillStyle("#ff00ff");
+    cvs.rect(Math.cos(this.elapsedTime)*50+window.innerWidth/2-30, window.innerHeight/2-30, 60, 60);
+    stats.end();
   }
 }
 
 let proj = new project();
-proj.bootstrap();
+//proj.bootstrap();
 
 let opt = {
   canvas: cvs
